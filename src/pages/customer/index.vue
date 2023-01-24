3fe7cc5dfffd540151cdd7be1099403b134c434b
Trian Damai <triannurizkillah@gmail.com>
2023-01-24 22:43:23 +0700

    initial

---
+++ src/pages/customer/index.vue
@@ -0,0 +1,29 @@
+
+<script setup lang="ts">
+
+const props = defineProps({})
+
+const headers = [
+  { title: 'Cover', align: 'end', sortable: false, key: 'cover' },
+  {
+    title: 'Judul',
+    align: 'start',
+    sortable: false,
+    key: 'name',
+  },
+  { title: 'ISBN', align: 'end', sortable: false, key: 'calories' },
+  { title: 'Aksi', align: 'end', sortable: false, key: 'action' },
+]
+
+const datas: Array<any> = []
+</script>
+<template>
+  <VRow>
+    <VCol cols="12">
+      <BookStatisticCard />
+    </VCol>
+    <VCol cols="12">
+      <BookDatatable :header="headers" :data="datas" />
+    </VCol>
+  </VRow>
+</template>
